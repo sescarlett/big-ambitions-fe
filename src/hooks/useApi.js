a0643593cfcd637c117c307425ced7d102932a5f
sscarlett <stephenscrltt@gmail.com>
2024-06-08 16:54:25 -0600

    github action

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const useApi = () => {
 
     return axios.create({
-        baseURL: 'http://localhost:8080/api',
+        baseURL: import.meta.env.VITE_BASE_WS_URL,
         headers: {
             'Content-Type': 'application/json',
         }
