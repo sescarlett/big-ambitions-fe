bed4a8525ea490b3a1cddf7eba41fa14aef6b591
sscarlett <stephenscrltt@gmail.com>
2024-06-13 22:05:49 -0600

    change api url

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const useApi = () => {
 
     return axios.create({
-        baseURL: 'https://big-ambitions-api-dlvg7ncywa-uc.a.run.app',
+        baseURL: 'https://big-ambitions-api-dlvg7ncywa-uc.a.run.app/api',
         headers: {
             'Content-Type': 'application/json',
         }
